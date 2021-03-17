46c6cd0cf08771a5fc2c39f19ff9b71c6538e600
bintangaria <bintangstarfreak27@gmail.com>
2021-03-17 20:49:44 +0700

    Initial Commit

---
+++ Weathim/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weathim
+//
+//  Created by Bintang Aria Ramadhan on 17/03/21.
+//  Copyright © 2021 Starfreak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
